8fd41b5efc193a72251aa09dc724082543dd8445
JD Angerhofer <jd.angerhofer@gmail.com>
2024-05-16 23:21:47 -0400

    [Number Cross 4]: Set up given example

--- number_cross_4/rust/src/main.rs
+++ number_cross_4/rust/src/main.rs
@@ -5,14 +5,15 @@ mod region;
 use crate::grid::Grid;
 
 fn main() {
-    let dimension = 4;
+    let dimension = 5;
 
     // Initial regions configuration
     let initial_regions = vec![
-        vec![(0, 0), (0, 1), (1, 0), (1, 1)],
-        vec![(2, 2), (2, 3), (3, 2), (3, 3)],
-        vec![(0, 2), (0, 3), (1, 2), (1, 3)],
-        vec![(2, 0), (2, 1), (3, 0), (3, 1)],
+        vec![(0, 0), (1, 0), (2, 0), (3, 0), (4, 0)],
+        vec![(0, 1), (1, 1), (2, 1), (3,1), (4, 1), (3, 2), (4, 2), ],
+        vec![(0, 2), (1, 2), (2, 2), (2, 3), (3, 3),(4,3), (4, 4), ],
+        vec![(0,3)],
+        vec![(1, 3), (0,4), (1, 4), (2, 4), (3, 4),],
     ];
 
     match Grid::new(dimension, initial_regions) {
